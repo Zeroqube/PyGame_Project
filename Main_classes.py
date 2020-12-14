4c673818ab48d1ff5f62c06636403e941766eaa6
Администратор <maxbystrov2006@yandex.ru>
2020-12-14 17:11:03 +0300

    Base_files

---
+++ Main_classes.py
@@ -0,0 +1,15 @@
+class board():
+    def __init__(self):
+        pass
+
+    def find_structure(self, x, y):
+        pass
+
+    def action_on_block(self, x, y, act):
+        pass
+
+    def create_structure(self, structure):
+        pass
+
+    def update_resource(self):
+        pass
