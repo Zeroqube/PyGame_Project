b05a551115f97afe2d66963862b796c27e4f109e
Zeroqube <maxbystrov2006@yandex.ru>
2020-12-28 11:26:09 +0300

    sprites

--- Main_classes.py
+++ Main_classes.py
@@ -1,15 +1,162 @@
-class board():
+import pygame
+import os
+import sys
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+pygame.init()
+screen_size = (400, 400)
+screen = pygame.display.set_mode(screen_size)
+FPS = 50
+
+player_image = load_image('glaider.png')
+
+tile_width = tile_height = 50
+
+
+class SpriteGroup(pygame.sprite.Group):
     def __init__(self):
+        super().__init__()
+
+    def get_event(self, event):
+        for sprite in self:
+            sprite.get_event(event)
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, group):
+        super().__init__(group)
+        self.rect = None
+
+    def get_event(self, event):
         pass
 
-    def find_structure(self, x, y):
-        pass
 
-    def action_on_block(self, x, y, act):
-        pass
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(sprite_group)
+        self.image = tile_type + '.png'
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
 
-    def create_structure(self, structure):
-        pass
 
-    def update_resource(self):
-        pass
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(hero_group)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.pos = (pos_x, pos_y)
+
+    def move(self, x, y):
+        self.pos = (x, y)
+
+
+player = None
+running = True
+clock = pygame.time.Clock()
+sprite_group = SpriteGroup()
+hero_group = SpriteGroup()
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["Перемещение героя", "",
+                  "",
+                  "Камера"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), screen_size)
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            Tile(level[x][y], x, y)
+    new_player = Player(0, 0)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+def move(hero, movement):
+    x, y = hero.pos
+    if movement == 'up':
+        if y > 0 and level_map[y - 1][x] == '.':
+            hero.move(x, y - 1)
+    elif movement == 'down':
+        if y < max_y - 1 and level_map[y + 1][x] == '.':
+            hero.move(x, y + 1)
+    elif movement == 'left':
+        if x > 0 and level_map[y][x - 1] == '.':
+            hero.move(x - 1, y)
+    elif movement == 'right':
+        if y < max_x and level_map[y][x + 1] == '.':
+            hero.move(x + 1, y)
+
+
+if __name__ == '__main__':
+    start_screen()
+    level_map = load_level('map.map')
+    hero, max_x, max_y = generate_level(level_map)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    move(hero, 'up')
+                elif event.key == pygame.K_DOWN:
+                    move(hero, 'down')
+                elif event.key == pygame.K_LEFT:
+                    move(hero, 'left')
+                elif event.key == pygame.K_RIGHT:
+                    move(hero, 'right')
+        screen.fill(pygame.Color('black'))
+        sprite_group.draw(screen)
+        hero_group.draw(screen)
+        clock.tick(FPS)
+        pygame.display.flip()
+    pygame.quit()
